ffe1a2ed8fee39d0816b18f9851de52826564a82
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 13:45:38 -0500

    ELI: refactoriza decode-> decodificarDigito

--- src/com/eli/SumaDeCaracteres/Main.java
+++ src/com/eli/SumaDeCaracteres/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         char[] digitos ={'0','1','2','3','4','5','6','7','8','9'};
 
-        //int num= decode('1');
+        //int num= decodificarDigito('1');
         //System.out.println("num = " + num);
 
         long suma = sumarDigitos(digitos);
@@ -14,7 +14,7 @@ public class Main {
 
     }
 
-    public static int decode(char digito){
+    public static int decodificarDigito(char digito){
         int num=digito-48;
         return num;
     }
@@ -22,7 +22,7 @@ public class Main {
     public static long sumarDigitos(char[] digitos){
         long suma =0;
         for( char digito : digitos){
-            suma+= decode(digito);
+            suma+= decodificarDigito(digito);
         }
         return suma;
     }
