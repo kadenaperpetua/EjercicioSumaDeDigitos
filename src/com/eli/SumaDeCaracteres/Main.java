e2ba9e23664b29d64ac8cdad8e2706fd37027ca2
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 13:11:15 -0500

    ELI: Agrega arreglo de caracteres con los digitos del 0 al 9

--- src/com/eli/SumaDeCaracteres/Main.java
+++ src/com/eli/SumaDeCaracteres/Main.java
@@ -3,6 +3,7 @@ package com.eli.SumaDeCaracteres;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char[] digitos ={'0','1','2','3','4','5','6','7','8','9'};
+        
     }
 }
