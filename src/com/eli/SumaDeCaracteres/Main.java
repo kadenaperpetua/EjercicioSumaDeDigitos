57c0a844f4e04621ef84050902b140f986a58f4d
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 13:25:59 -0500

    ELI: Agrega funcion sumarDigitos

--- src/com/eli/SumaDeCaracteres/Main.java
+++ src/com/eli/SumaDeCaracteres/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
         char[] digitos ={'0','1','2','3','4','5','6','7','8','9'};
 
-        int num= decode('1');
-        System.out.println("num = " + num);
+        //int num= decode('1');
+        //System.out.println("num = " + num);
+
+        long suma = sumarDigitos(digitos);
+        System.out.println("suma = " + suma);
+
 
     }
 
@@ -15,5 +19,12 @@ public class Main {
         return num;
     }
 
+    public static long sumarDigitos(char[] digitos){
+        long suma =0;
+        for(digito : digitos){
+            suma+= decode(char digito);
+            return suma;
+        }
+    }
 
 }
