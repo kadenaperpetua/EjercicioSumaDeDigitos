6e819a0aee0aed2b4af167bb8d4a13d8346e7d2e
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 13:15:52 -0500

    ELI: Agrega funcion decode que transforma el caracter en entero

--- src/com/eli/SumaDeCaracteres/Main.java
+++ src/com/eli/SumaDeCaracteres/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         char[] digitos ={'0','1','2','3','4','5','6','7','8','9'};
-        
+
+        int num= decode('1');
+        System.out.println("num = " + num);
+
     }
+
+    public static int decode(char digito){
+        int num=digito-48;
+        return num;
+    }
+
+
 }
